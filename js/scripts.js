8b33ce44daca57891d653332fcb033b68a75c962
Nigel Orion <Guest@epicodus-14.local>
2016-10-28 09:23:29 -0700

    Add JS, Jquery to take user input and convert to a number count array

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,57 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function(){
-  $("form").submit(function(event){
+$(document).ready(function() {
+  $("form").submit(function(event) {
     event.preventDefault();
-    var userNumber = $("#number-input").val();
+    var userNumber = parseInt($("#number-input").val());
+    errorCheck(userNumber);
+    arrayCount(userNumber);
     console.log(userNumber);
-
-
-
 
   });
 });
+
+
+
+
+
+
+
+
+function errorCheck (userNumber) {
+
+  if (!userNumber){
+    alert("Enter a number!");
+    return false;
+  } else if (userNumber < 0) {
+    alert("Please no negative numbers");
+    return false;
+  } else if (isNaN(userNumber)) {
+    alert("Please enter numbers only, no letters!");
+    return false;
+  } else {
+    return true;
+  }
+};
+
+function arrayCount (userNumber) {
+
+  var userArray = [];
+
+  for (i = 0; i < userNumber; i++) {
+    userArray.push(i);
+  }
+  return userArray;
+  console.log(userArray);
+};
+
+
+//
+//  function counter (userNumber) {
+//
+//   var pingPongCount = [];
+//
+//   for (i = 0; i = userNumber; i++) {
+//     pingPongCount.push[i];
+//   }
+//   return pingPongCount;
+//
+// };
